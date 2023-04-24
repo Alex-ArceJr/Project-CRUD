19629f451b675b4796ba8f69a7d12f00f79b2364
Alex-Arce <arcealejandrino@gmail.com>
2023-04-23 21:20:09 -0700

    made book model and schema

---
+++ models/book.js
@@ -0,0 +1,42 @@
+//require mongoose
+const mongoose = require('mongoose');
+//setup the schema for our model
+const Schema = mongoose.Schema;
+
+
+const reviewSchema = new Schema ({
+  content: {
+    type: String
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10
+  }
+});
+
+const bookSchema = new Schema ({
+  author: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  imgURL: {
+    type: String
+  },
+  price: {
+    type: number
+  },
+  isbn: {
+    type: number,
+    required: true
+  },
+  reviews: [reviewSchema]
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Book', bookSchema)
