54753ad45d2db65a59b4554b0ec94a076bf4a756
nadiras <nadiraomar98@gmail.com>
2023-04-27 03:43:30 -0500

    wip: added styling to home, refactored code, and fixed styling to new

--- controllers/home.js
+++ controllers/home.js
@@ -6,29 +6,14 @@ async function home(req, res) {
   try{
     const allBooks = await Book.find({})
 
-    const genreFiction = await Book.find({genre: 'Fiction'})
-    const genreNonfiction = await Book.find({genre: 'Non-fiction'})
-    const genreMystery = await Book.find({genre: 'Mystery'})
-    const genreAutobiography = await Book.find({genre: 'Autobiography'})
-    const genreOther = await Book.find({genre: 'Other'})
     
-    res.render('home', {title: 'Book Library', books: allBooks, fiction: genreFiction, nonfiction: genreNonfiction, mystery: genreMystery, autobio: genreAutobiography, other: genreOther})
+    res.render('home', {title: 'Book Library', books: allBooks})
 
   } catch(error) {
     console.log(error)
   }
 
 }
-
-// async function show(req, res) {
-//   try {
-//     const foundBook = await Book.findById(req.params.id)
-//     res.redirect('home',{book: foundBook})
-
-//   } catch(error) {
-//     console.log(error)
-//   }
-// }
 
 //export module
 module.exports = {
